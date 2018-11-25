89a40e4f078fab586afe757f25e02dd650a432be
root <root@eosnode-ufm.c.conulta-eos.internal>
2018-11-25 21:24:23 +0000

    whatever

--- pythonapi/venv/bin/activate.fish
+++ pythonapi/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/juanllopez/pythonapi/venv"
+set -gx VIRTUAL_ENV "/home/juanllopez/changeos/pythonapi/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
