30f7cb5eed1ccfbad8d35428ec6c7aef5dfa2364
Juanro Alvarado <juanroalvarado@Juanros-MacBook-Pro.local>
2018-11-24 23:32:03 -0600

    python ipfs calls

---
+++ ipfs-python/ipfs.py
@@ -0,0 +1,19 @@
+import ipfsapi
+
+
+def hashImage(api, bytecode):
+    res = api.add(bytecode)
+    print(res)
+    return res
+
+def getImage(api, hash):
+    img = api.cat(hash)
+    print(type(img))
+    return img
+
+
+if __name__ == "__main__":
+    api = ipfsapi.connect('127.0.0.1', 5001)
+
+#hs = hashImage(api,'dpi_0.png')
+#getImage(api, 'QmdodXR3WWQGxmUmQsZy9PjuzYuUwAiEgCMif92ChLq2sJ')
